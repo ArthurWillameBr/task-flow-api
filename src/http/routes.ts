db520e544a6db6994cdf68e512d61dc318bd8c04
arthurwillame5 <arthurwillame2005@gmail.com>
2024-08-24 12:29:49 -0300

    feat: add session authentication endpoint

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { register } from "./controller/register";
+import { authenticate } from "./controller/authenticate";
 
 const router: Router = Router();
 
 router.post("/users", register)
+router.post("/sessions", authenticate)
 
 export default router
