e70e13e0609a1260fc7f4463dbccf5340e9c521f
arthurwillame5 <arthurwillame2005@gmail.com>
2024-08-24 09:51:13 -0300

    feat: add environment variable validation

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import { Request, Response } from "express";
 import { app } from "./app";
+import { env } from "./env";
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello World")
 })
 
-app.listen(3000, () => {
-    console.log("🚀 HTTP Server Running!")
+app.listen(env.PORT, () => {
+    console.log(`🚀 HTTP Server Running on Port ${env.PORT}`)
 })
