9379bc64f7f6e4d8bbe2d0b44e286c54d33a2c79
arthurwillame5 <arthurwillame2005@gmail.com>
2024-08-24 13:03:52 -0300

    feat: implement JWT token generation in authenticate controller

--- src/http/controller/authenticate.ts
+++ src/http/controller/authenticate.ts
@@ -1,32 +1,40 @@
+import { env } from "@/env";
 import { PrismaUserRepository } from "@/repositores/prisma/prisma-user-repository";
 import { AuthenticateUseCase } from "@/use-cases/authenticate";
 import { InvalidCredentialsError } from "@/use-cases/errors/invalid-credentials-error";
 import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
 import { z } from "zod";
 
 export async function authenticate(req: Request, res: Response) {
-    const authenticateBodySchema = z.object({
-        email: z.string().email(),
-        password: z.string().min(6)
-    })
+  const authenticateBodySchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6),
+  });
 
-    const {email, password} = authenticateBodySchema.parse(req.body)
+  const { email, password } = authenticateBodySchema.parse(req.body);
 
-    try {
-        const usersRepository = new PrismaUserRepository()
-        const authenticateUseCase = new AuthenticateUseCase(usersRepository)
+  try {
+    const usersRepository = new PrismaUserRepository();
+    const authenticateUseCase = new AuthenticateUseCase(usersRepository);
 
-        await authenticateUseCase.execute({
-            email,
-            password
-        })
+    const { user } = await authenticateUseCase.execute({
+      email,
+      password,
+    });
 
-    } catch (error) {
-        if(error instanceof InvalidCredentialsError) {
-            return res.status(400).send({message: error.message})
-        }
-        throw error
+    const token = jwt.sign({ sub: user.id }, env.JWT_SECRET, {
+      expiresIn: "30d",
+    });
+
+    return res.status(200).send({
+        token
+    });
+    
+  } catch (error) {
+    if (error instanceof InvalidCredentialsError) {
+      return res.status(400).send({ message: error.message });
     }
-
-    return res.status(200).send({message: "Authenticated"})
+    throw error;
+  }
 }
