ae61d5db00ee7a17e1bcff8b2b1ecd3531a067e9
arthurwillame5 <arthurwillame2005@gmail.com>
2024-09-01 12:52:47 -0300

    chore: cors options

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,13 @@ import cors from "cors"
 
 export const app = express()
 
-app.use(cors())
+const corsOptions = {
+    origin: "https://task-flow-web-delta.vercel.app/", 
+    optionsSuccessStatus: 200 
+  };
+  
+app.use(cors(corsOptions));
+
 app.use(express.json());
 
 app.use("/api", router)
